b43ec54eb4ce49be358cb6e6058913c39c8d60f8
= <=>
2022-09-14 18:03:38 -0700

    Update branch with latest changes in master

---
+++ src/main/java/com/thompetrus/githubexercise/GithubExerciseApplication.java
@@ -0,0 +1,15 @@
+package com.thompetrus.githubexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(exclude = { RedisAutoConfiguration.class } )
+public class GithubExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubExerciseApplication.class, args);
+	}
+
+}
